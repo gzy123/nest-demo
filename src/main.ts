3be6a071a94f24383ad36580bb7575d6a5f4a2cc
owen <448950802@qq.com>
2023-01-20 10:01:53 +0800

    feat: 添加全局异常过滤器

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-
+import { HttpExceptionFilter } from './http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   const config = new DocumentBuilder()
     .setTitle('this is a title')
     .setDescription('this is a description')
